c15bde143dd8b8d5d8d0ac22cba71337434249c2
Aadarsh <aadarshhanu23@gmail.com>
2023-04-01 22:28:28 +0530

    school

---
+++ Print Elements of Array.cpp
@@ -0,0 +1,13 @@
+//https://practice.geeksforgeeks.org/problems/print-elements-of-array4910/0
+//Print Elements of Array
+
+//User function template for C++
+class Solution{
+public:
+    //Just print the space seperated array elements
+	void printArray(int arr[], int n) {
+	    // code here
+	    for(int i=0;i<n;i++)
+	    cout<<arr[i]<<" ";
+	}
+};
