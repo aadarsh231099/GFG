8606b03f3bd83c476edc5046b08c5d343309469e
Aadarsh <aadarshhanu23@gmail.com>
2023-04-01 22:36:05 +0530

    school

---
+++ C++ Array (print an element)  Set 2.cpp
@@ -0,0 +1,11 @@
+//https://practice.geeksforgeeks.org/problems/c-array-print-an-element-set-25933/0
+//C++ Array (print an element) | Set 2
+
+class Solution{
+    public:
+    int findElementAtIndex(int a[], int n, int key) 
+    {
+        // Your code goes here
+        return a[key];
+    }
+};
