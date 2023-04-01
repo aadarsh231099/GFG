eebcda51b79889636ceb42f00f10b7810e356c03
Aadarsh <aadarshhanu23@gmail.com>
2023-04-01 22:57:45 +0530

    medium

---
+++ Kth smallest element.cpp
@@ -0,0 +1,20 @@
+//https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/0
+//Kth smallest element
+
+
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int n=r+1;
+        sort(arr,arr+n);
+        return arr[k-1];
+        
+    }
+};
