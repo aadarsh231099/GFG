46325f061814aa6321eabb2b93c140be9d2444fe
Aadarsh <aadarshhanu23@gmail.com>
2023-04-01 22:33:00 +0530

    school

---
+++ Print alternate elements of an array.cpp
@@ -0,0 +1,17 @@
+//https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/0
+//Print alternate elements of an array
+
+//User function Template for C++
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    
+    // code here
+    for(int i=0;i<n;i+=2)
+    cout<<ar[i]<<" ";
+    
+    
+    
+}
