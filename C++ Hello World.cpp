c5809b5c3c07d45124f4d0142d06f551e98dfa21
Aadarsh <aadarshhanu23@gmail.com>
2023-04-03 16:15:46 +0530

    basic

---
+++ C++ Hello World.cpp
@@ -0,0 +1,12 @@
+//https://practice.geeksforgeeks.org/problems/c-hello-world3402/0
+//C++ Hello World
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        cout<<"Hello World";
+    }
+};
