58d4bda8dd7b923a8b6bd5762fb56cde2e995001
Aadarsh <aadarshhanu23@gmail.com>
2023-04-03 15:18:53 +0530

    basic

---
+++ Sort a String.cpp
@@ -0,0 +1,10 @@
+//https://practice.geeksforgeeks.org/problems/sort-a-string2943/0
+//Sort a String
+
+
+
+string sort(string s){
+    //complete the function here
+    sort(s.begin(),s.end());
+    return s;
+}
