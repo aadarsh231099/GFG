ec338ecb9359ed003e4227a131d8fbe987ebac6e
Aadarsh <aadarshhanu23@gmail.com>
2023-04-04 12:38:53 +0530

    basic

---
+++ Identical Linked Lists.cpp
@@ -0,0 +1,29 @@
+//https://practice.geeksforgeeks.org/problems/identical-linked-lists/0
+//Identical Linked Lists
+
+
+
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to check whether two linked lists are identical or not. 
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    // Code here
+    while(head1!=NULL && head2!=NULL)
+    {
+        if(head1->data!=head2->data)
+            return false;
+        head1=head1->next;
+        head2=head2->next;
+    }return(head1==NULL && head2==NULL);
+}
