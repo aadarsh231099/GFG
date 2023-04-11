f0788aa4490ab4029a1eeb9dbcb18ab26b75e002
Aadarsh <aadarshhanu23@gmail.com>
2023-04-11 12:04:06 +0530

    easy

---
+++ Segregate 0s and 1s.cpp
@@ -0,0 +1,12 @@
+//https://practice.geeksforgeeks.org/problems/segregate-0s-and-1s5106/0
+//Segregate 0s and 1s
+
+//User function template for C++
+
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        sort(arr,arr+n);
+    }
+};
