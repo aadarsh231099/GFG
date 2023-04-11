059529dfed19bf15e469e3a900973c00ee28c457
Aadarsh <aadarshhanu23@gmail.com>
2023-04-11 14:25:36 +0530

    hard

---
+++ HARD/Bit Difference.cpp
@@ -0,0 +1,32 @@
+//https://practice.geeksforgeeks.org/problems/find-sum-of-different-corresponding-bits-for-all-pairs4652/0
+//Bit Difference
+
+// User function Template for C++
+
+class Solution{
+public:
+    int countBits(int n, long long int a[])
+    {
+        // code here
+        const int mod=1e9+7;
+        // Your logic here
+                vector<int> v(32,0);
+        for(int j=0;j<n;j++){
+            int i=0,element=a[j];
+            while(element){
+                v[i++]+=(element&1);
+                element>>=1;
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int val=a[i];
+            for(int j=0;j<32;j++){
+                if(val&1) cnt=(cnt+n-v[j])%mod;
+                else cnt=(cnt+v[j])%mod;
+                val>>=1;
+            }
+        }
+        return cnt%mod;
+    }
+};
