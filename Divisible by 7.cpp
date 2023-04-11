b823c1ab835f405a0fd3c469efb90fc539f94417
Aadarsh <aadarshhanu23@gmail.com>
2023-04-11 12:06:28 +0530

    easy

---
+++ Divisible by 7.cpp
@@ -0,0 +1,31 @@
+//https://practice.geeksforgeeks.org/problems/divisible-by-73224/0
+//Divisible by 7
+
+//User function template for C++
+
+class Solution{
+  public:
+    int isdivisible7(string num){
+        
+        //complete the function here
+        int n=num.length();
+        if(n==0)return 1;
+        if(n%3==1)
+        {num="00"+num;
+        n+=2;
+        }
+        else if(n%3==2){
+            num="0"+num;
+            n++;
+        }
+        int gsum=0,p=1;
+        for(int i=n-1;i>=0;i--)
+        {   int sum=0;
+            sum+=num[i--]-'0';
+            sum+=(num[i--]-'0')*10;
+            sum+=(num[i]-'0')*100;
+            gsum=gsum+sum*p;
+            p*=-1;
+        }return (gsum%7==0);
+    }
+};
