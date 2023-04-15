be25e9987846e6ff6a07a1e10b4b4a9e8695a139
Aadarsh <aadarshhanu23@gmail.com>
2023-04-15 10:50:05 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        int max=0,pos=0;
+        for(int i=0;i<n;i++){
+        if(names[i].length()>max)
+        {
+            max=names[i].length();
+            pos=i;
+        }
+        }return names[pos];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
